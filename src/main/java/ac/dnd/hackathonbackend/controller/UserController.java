2ad6d7e8c44f516f57e20a3f13d5c1c27b25f1f5
Jinseong Hwang <jinseong.dev@gmail.com>
2021-11-20 00:49:17 +0900

    chore: 컨트롤러 패키지 추가

---
+++ src/main/java/ac/dnd/hackathonbackend/controller/UserController.java
@@ -0,0 +1,15 @@
+package ac.dnd.hackathonbackend.controller;
+
+import ac.dnd.hackathonbackend.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+}
