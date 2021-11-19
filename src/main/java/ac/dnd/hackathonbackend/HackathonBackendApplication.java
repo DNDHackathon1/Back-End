2bffdd4db989abe9ae4ae81e1d92443ffd97994e
Jinseong Hwang <jinseong.dev@gmail.com>
2021-11-20 03:32:01 +0900

    feat: JPA Auditing 기능 추가

--- src/main/java/ac/dnd/hackathonbackend/HackathonBackendApplication.java
+++ src/main/java/ac/dnd/hackathonbackend/HackathonBackendApplication.java
@@ -2,7 +2,9 @@ package ac.dnd.hackathonbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HackathonBackendApplication {
 
